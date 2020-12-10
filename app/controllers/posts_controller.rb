6e2628498cf390ececcf45dc29303b1480d69513
alansoto31 <soto.alan@hotmail.com>
2020-12-10 15:23:45 -0600

    Remove logic from views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,10 +17,8 @@ class PostsController < ApplicationController
 
     if @post.save
       flash.notice = 'Post created!!!'
-      # flash[:notice] = "Post successfully created"
       redirect_to posts_url
     else
-      # flash[:alert] = "Something went wrong"
       render 'new'
     end
   end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,26 @@
 module PostsHelper
+  attr_reader :posts
+  attr_writer :post_name, :author, :so_button, :si_button, :su_button, :msg_class
+
   def post_params
     params.require(:post).permit(:title, :body, :user_id)
+  end
+
+  def u_signin
+    return unless user_signed_in?
+
+    @so_button = link_to 'Sign Out', destroy_user_session_path, method: 'Delete', class: 'btn btn-info'
+  end
+
+  def user_si
+    if user_signed_in?
+      @author = 'Author'
+      @posts.each do |post|
+        @post_name = post.user.name
+      end
+    else
+      @si_button = link_to 'Sign in', user_session_path, class: 'btn btn-info'
+      @su_button = link_to 'Sign Up', new_user_registration_path, class: 'btn btn-primary'
+    end
   end
 end
